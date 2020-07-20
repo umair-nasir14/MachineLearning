c063882e258d6fc685e07dd8133830da98dc399d
umair-nasir14 <68095790+umair-nasir14@users.noreply.github.com>
2020-07-20 15:09:34 +0500

    Update Hidden_Markov_Model.py

--- Hidden_Markov_Model.py
+++ Hidden_Markov_Model.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 
 #defining transitions and observations
 
-initial_distribution = tfp.distributions.Categorical(probs=[0.2, 0.8]) #fisrt day is 80% cold
-transition_distribution = tfp.distributions.Categorical(probs=[[0.3, 0.7],
+initial_distribution = tfp.distributions.Categorical(probs=[0.8, 0.2]) #fisrt day is 80% cold
+transition_distribution = tfp.distributions.Categorical(probs=[[0.7, 0.3],
                                                  [0.2, 0.8]])  #Cold day has 30%  chance of being followed by a hot day and a hot day has 20% chance of being followed by a cold day
 observation_distribution = tfp.distributions.Normal(loc=[0., 15.], scale=[5., 10.])  #the loc argument represents the mean and the scale is the standard devitation. FIrst is cold and then hot day.
 
